d6ddcb7c1b8d27bed20d683592f649e6f058703d
AntonKloch <166872960+AntonKloch@users.noreply.github.com>
2024-04-17 22:16:56 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,17 @@
+# food = ['coconut','banana',"cherry",'apple','peach']
+# print (food)
+# print(type(food))
+# print(food[0])
+# print(food[-1])
+# print(food[2:5])
+# food[2]="fish"
+# print(food)
+
+logPar ={'Milana':'ghg4h5g4h5g','Margarita':'tr7t8r7e87g4','Anton':'cvxcvc4c5v4cvc',
+          'Lydmila':'u7u8i7uy87iuy87i'
+}
+print(logPar)
+print(logPar['Milana'])
+logPar['Milana'] = 'fg8f9d865g'
+print(logPar)
+
