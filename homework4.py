5afafcb762981abc5ecb60514dafa22c531ebb96
AntonKloch <166872960+AntonKloch@users.noreply.github.com>
2024-04-17 20:17:55 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,11 @@
+tuple=(1,2.5,'true',"Anton",)
+print(tuple)
+print(type(tuple))
+list1 = [1,2,3,89,112,61,87,'banana',99.6,88,'hello',"false"]
+print(list1)
+print(type(list1))
+list1.append('69')
+print(list1)
+
+
+
