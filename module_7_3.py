e181f7e1f09b92efafb48f1b61ef966b52083ce6
AntonKloch <166872960+AntonKloch@users.noreply.github.com>
2024-10-05 15:32:48 +0300

    Add files via upload

---
+++ module_7_3.py
@@ -0,0 +1,42 @@
+import io
+from pprint import pprint
+
+class WordsFinder:
+    def __init__(self,*files_name):
+        self.files_name = files_name
+
+    def get_all_words(self):
+        all_words = {}
+        for file_name in self.files_name:
+            with open(file_name, 'r', encoding='utf-8') as file:
+                text = file.read().lower()
+                for punct in [',', '.', '=', '!', '?', ';', ':', ' - ']:
+                    text = text.replace(punct, '')
+                words = text.split()
+                all_words[file] = words
+        return all_words
+
+    def find(self, word):
+        all_words = self.get_all_words()
+        result = {}
+        for file_name, words in all_words.items():
+            for i, w in enumerate(words):
+                if w == word:
+                    result[file_name] = i
+        return result
+
+    def count(self, word):
+        all_words = self.get_all_words()
+        result = {}
+        for file_name, words in all_words.items():
+            result[file_name] = words.count(word)
+        return result
+
+finder2 = WordsFinder('Rudyard Kipling - If.txt')
+print(finder2.get_all_words())  # Все слова
+print(finder2.find('TEXT'))  # 3 слово по счёту
+print(finder2.count('if'))  # 4 слова teXT в тексте всего
+
+
+
+
